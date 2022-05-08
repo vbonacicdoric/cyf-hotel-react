a43c585322e600b3a14abf39e1a2b387dad0586f
Valeria Bonacic Doric <94327965+vbonacicdoric@users.noreply.github.com>
2022-05-09 00:21:17 +0200

    punto dos completo falta arreglos en el css de la img

---
+++ src/components/Heading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <header className="App-header">
+      <img
+        class="Logo-Hotel"
+        src="https://i.pinimg.com/600x315/de/f7/b6/def7b694904830d5804ee5975b69e9ed.jpg"
+      />
+      <p>CYF Hotel</p>
+    </header>
+  );
+};
+
+export default Heading;
